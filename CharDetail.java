c776bb5e638b5d3a6c5dea42a749d4a5d5e2e658
DESKTOP-9V7RBBE\H <hamit196971@gmail.com>
2023-03-03 19:18:34 +0800

    CharDetail

---
+++ CharDetail.java
@@ -0,0 +1,17 @@
+public class CharDetail {
+	public static void main(String[] args) {
+		char c1 = 97;
+		System.out.println(c1);
+		
+		char c2 = 'a';
+		System.out.println(c2);
+		
+		char c3 = '韩';
+		System.out.println(c3);
+		
+		char c4 = 38889;
+		System.out.println(c4);
+		
+		System.out.println('a' + 10);
+	}
+}
