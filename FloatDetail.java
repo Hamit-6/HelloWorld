a7d8c69aa448fa5edfb6e7cad02865fb7fda10ed
DESKTOP-9V7RBBE\H <hamit196971@gmail.com>
2023-03-03 19:19:05 +0800

    FloatDetail

---
+++ FloatDetail.java
@@ -0,0 +1,20 @@
+public class FloatDetail {
+	public static void main(String[] args ) {
+		float num1 = 1.1f;
+		double num2 = 2.1;
+		double num3 = 3.1f;
+		double num4 = .321;
+		System.out.println(num4);
+		System.out.println(5.12e2);
+		System.out.println(5.12e-2);
+		double num9 = 2.1234567851;
+		float num10 = 2.1234567851f;
+		System.out.println(num9);
+		System.out.println(num10);
+		
+		double num11 = 2.7;
+		double num12 = 8.1/3;
+		System.out.println(num11);
+		System.out.println(num12);
+    }
+}	
