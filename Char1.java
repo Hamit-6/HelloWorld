87dcb224c2166f06b5a8da76f489cab1d81686f4
DESKTOP-9V7RBBE\H <hamit196971@gmail.com>
2023-03-11 18:36:29 +0800

    Char1

---
+++ Char1.java
@@ -0,0 +1,12 @@
+public class Char1 {
+	public static void main(String[] args ) {
+		char c1 = 'a';
+		char c2 = '\t';
+		char c3 = '海';
+		char c4 = 90;
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(c3);
+		System.out.println(c4);
+	}
+}
